e967a08715523638df1a43ca64575be32e0b143d
Kevin Chou <nbperson@163.com>
2015-12-22 23:14:24 +0800

    modify demo

--- lineButton/lineButton/ViewController.m
+++ lineButton/lineButton/ViewController.m
@@ -9,34 +9,62 @@
 #import "ViewController.h"
 #import "NCLineButton.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
+    NSArray *titles = @[@"Object-C",@"Swift",@"Java",@"C++",@"Python",@"Perl",@"Ruby"];
+    
+    NSArray *colors = @[@0xda0018,@0xdd1d18,@0xdf3b18,@0xe25819,@0xe4751b,@0xe7921b,@0xebaf1b];
+    
+    __block int y = 100;
 
-    NCLineButton *lineButton = [[NCLineButton alloc] initWithFrame:CGRectMake(100, 100, 100, 40)];
-    
-    lineButton.backgroundColor = [UIColor grayColor];
-    
-    [lineButton setTitle:@"dsdfsdfsdfdsfs" forState:UIControlStateNormal];
-    
-    [lineButton setColor:[UIColor blueColor] forState:UIControlStateSelected];
-//
-//    [lineButton setColor:[UIColor blueColor] forState:UIControlStateHighlighted];
-
-    
-    [lineButton addTarget:self action:@selector(testButton:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:lineButton];
+    [titles enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        
+        UIColor *color = UIColorFromRGB([colors[idx] intValue]);
+        
+        NCLineButton *button = [self lineButton:obj color:color y:y];
+        
+        y = CGRectGetMaxY(button.frame) + 20;
+        
+        [self.view addSubview:button];
+        
+    }];
 }
 
-- (void)testButton:(NCLineButton *)button
+- (void)buttonClicked:(NCLineButton *)button
 {
-    button.selected = YES;
+    
+}
+
+- (NCLineButton *)lineButton:(NSString *)title color:(UIColor *)color y:(int)y
+{
+    int width = 160;
+    
+    int x = (self.view.frame.size.width - width) / 2;
+    
+    NCLineButton *button = [[NCLineButton alloc] initWithFrame:CGRectMake(x, y, width, 40)];
+    
+    [button setTitle:title forState:UIControlStateNormal];
+    
+    [button setColor:color forState:UIControlStateNormal];
+    
+    [button setBackgroundColor:color forState:UIControlStateHighlighted];
+
+    [button setColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    
+    [button addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    return button;
 }
 
 - (void)didReceiveMemoryWarning {
